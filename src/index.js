4313ad40bed02390ad285656e12b52badc054f5e
fahad <fjkiani@icloud.com>
2020-04-27 06:33:02 -0400

    c

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.js';
-import Spinner from 'react-bootstrap/Spinner'
+//
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
 
   </React.StrictMode>,
   document.getElementById('root')
