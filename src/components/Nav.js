87bcf41a6b25306f5ae46c13c5ac1148afae24da
fahad <fjkiani@icloud.com>
2020-04-27 06:45:27 -0400

    c

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -25,7 +25,7 @@ export default function NavBar() {
              <Nav.Link href="/">Home</Nav.Link>
         </Nav.Item>
          <Nav.Item>
-              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/About">About</Nav.Link>
        </Nav.Item> 
 
         <Nav.Item>
